c5ecee172178212aa593ae6f22e3cf5df8f488a6
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-08-03 17:48:43 -0300

    Adição logo a Landing page

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 
+import logoImg from '../../assets/images/logo.svg'
+
 const Landing: React.FC = () => {
   return (
-    <div>
-      <h1>Landing</h1>
+    <div id="page-landing">
+      <div id="page-landing-content" className="container">
+        <div className="logo-container">
+          <img src={logoImg} alt="Proffy"/>
+        </div>
+      </div>
     </div>
   )
 }
