e966716594f8dd20b49ba62ec8e028814de1886c
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-08-03 18:05:04 -0300

    Adição novos ícones

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 
 import logoImg from '../../assets/images/logo.svg'
+import landingImg from '../../assets/images/landing.svg'
+
+import studyIcon from '../../assets/images/icons/study.svg'
+import giveClassesIcon from '../../assets/images/icons/give-classes.svg'
 
 const Landing: React.FC = () => {
   return (
@@ -8,6 +12,21 @@ const Landing: React.FC = () => {
       <div id="page-landing-content" className="container">
         <div className="logo-container">
           <img src={logoImg} alt="Proffy"/>
+
+          <h2>Sua plataforma de estudos online</h2>
+        </div>
+
+        <img src={landingImg} alt="Plataforma de estudos" className="hero-image"/>
+
+        <div className="buttons-container">
+          <a href="#" className="study">
+            <img src={studyIcon} alt="Estudar" />
+            Estudar
+          </a>
+          <a href="#" className="study">
+            <img src={giveClassesIcon} alt="Dar aulas" />
+            Dar aulas
+          </a>
         </div>
       </div>
     </div>
