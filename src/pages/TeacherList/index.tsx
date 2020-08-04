d5938574589c57eddcc5771a0606f0d03e65f108
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-08-04 18:44:53 -0300

    Adição estrutura main

--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 import PageHeader from '../../components/PageHeader';
 
+import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
+
 import './styles.css'
 
 const TeacherList: React.FC = () => {
@@ -25,6 +27,31 @@ const TeacherList: React.FC = () => {
           </div>
         </form>
       </PageHeader>
+
+      <main>
+        <article className="teacher-item">
+          <header>
+            <img src="https://avatars2.githubusercontent.com/u/52754546?s=460&u=beb81a6de4cfbea7677783e3ab2527e30582478d&v=4" alt="Emidio"/>
+            <div>
+              <strong>Vitor Emidio</strong>
+              <span>Matemática</span>
+            </div>
+          </header>
+          <p>
+            Amante por números e adora ensinar o que há de melhor com conhecimentos em geometria e trigonometria
+          </p>
+
+          <footer>
+            <p>
+              Preço/hora <strong>R$100,00</strong>
+            </p>
+            <button type="button">
+              <img src={whatsappIcon} alt="WhatsApp"/>
+              Entrar em contato
+            </button>
+          </footer>
+        </article>
+      </main>
     </div>
   )
 }
