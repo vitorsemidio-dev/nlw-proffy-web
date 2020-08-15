6a46ce19c472bced1f62c1d549accff7ff6ac618
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-08-15 16:46:03 -0300

    Estruturando html form

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,17 +1,34 @@
-import React from 'react';
+import React, { useCallback, FormEvent } from 'react';
 
 import landing from '../../assets/images/landing.svg';
 import success from '../../assets/images/success-background.svg';
 
+import { Container } from './styles';
+
 const Login: React.FC = () => {
+  const handleSubmit = useCallback((eventSumit: FormEvent) => {
+    eventSumit.preventDefault();
+    console.log('submit');
+  }, []);
+
   return (
-    <div>
-      <h1>Login</h1>
-      <div>
-        <img src={landing} alt="Landing" />
-        <img src={success} alt="success" />
-      </div>
-    </div>
+    <Container>
+      <h1>Fazer login</h1>
+
+      <form onSubmit={handleSubmit}>
+        <div className="inputs-form">
+          <input type="email" id="email" />
+          <input type="password" id="password" />
+        </div>
+
+        <div className="footer">
+          <input type="checkbox" name="Lembrar-me" id="remeber-me" />
+          <a href="/">Esqueci minha senha</a>
+        </div>
+
+        <button type="submit">Entrar</button>
+      </form>
+    </Container>
   );
 };
 
