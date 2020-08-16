7e76efd8f9ab89e203fe16cb61354b9cc6793e9a
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-08-15 23:33:32 -0300

    Estrutura inicial Register

--- src/pages/Login/styles.ts
+++ src/pages/Login/styles.ts
@@ -35,8 +35,6 @@ export const DescriptionContainer = styled.div`
 `;
 
 export const BackgroundImage = styled.div`
-  /* flex: 1; */
-  /* background-color: #8257e5; */
   width: 100%;
   height: 100vh;
   background: url(${backgroundImage}) no-repeat center;
---
+++ src/pages/Register/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import {
+  Container,
+  BackgroundContent,
+  BackgroundImage,
+  ContentForm,
+  FormContainer,
+} from './styles';
+
+const Register: React.FC = () => {
+  return (
+    <Container>
+      <BackgroundContent>
+        <BackgroundImage>texto</BackgroundImage>
+      </BackgroundContent>
+
+      <ContentForm>
+        <FormContainer />
+      </ContentForm>
+    </Container>
+  );
+};
+
+export default Register;
