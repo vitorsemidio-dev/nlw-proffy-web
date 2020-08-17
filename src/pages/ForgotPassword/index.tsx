44eb4f22288f57d82859b03744b8368afeb6fd1c
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-08-17 18:08:42 -0300

    Estrutura form forgot password

--- src/pages/ForgotPassword/index.tsx
+++ src/pages/ForgotPassword/index.tsx
@@ -1,14 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import Input from '../Login/Input';
 
 import BackgroundImage from '../../components/BackgroundImage';
 
-import { Container } from './styles';
+import { Container, ContentForm, FormContainer } from './styles';
 
 const ForgotPassword: React.FC = () => {
+  const [email, setEmail] = useState('');
   return (
     <Container>
       <BackgroundImage text="Sua plataforma de estudos online" />
-      <h1>haha</h1>
+
+      <ContentForm>
+        <FormContainer>
+          <fieldset>
+            <legend>
+              Eita, esqueceu sua senha?
+              <span>Não esquenta, vamos dá um jeito nisso.</span>
+            </legend>
+
+            <Input
+              fieldId="email"
+              type="email"
+              label="E-mail"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+
+            <button type="submit">Enviar</button>
+          </fieldset>
+        </FormContainer>
+      </ContentForm>
     </Container>
   );
 };
