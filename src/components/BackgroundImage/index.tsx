cb8f74f7ac0b9dcfda448369d709fd5aa205a5b0
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-08-17 17:30:05 -0300

    Criação componente Background Image

---
+++ src/components/BackgroundImage/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const BackgroundImage: React.FC = () => {
+  return (
+    <Container>
+      <h1>background image</h1>
+    </Container>
+  );
+};
+
+export default BackgroundImage;
