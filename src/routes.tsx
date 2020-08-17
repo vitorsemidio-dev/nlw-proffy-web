9a1e6acdb255e0d515b0872bcf52cf35d1433933
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-08-17 17:27:08 -0300

    Criação componente ForgoPassword

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-import Landing from './pages/Landing';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import ForgotPassword from './pages/ForgotPassword';
+
+import Landing from './pages/Landing';
 import TeacherForm from './pages/TeacherForm';
 import TeacherList from './pages/TeacherList';
 
@@ -14,6 +16,7 @@ const Routes: React.FC = () => {
         <Route path="/" exact component={Landing} />
         <Route path="/login" component={Login} />
         <Route path="/register" component={Register} />
+        <Route path="/forgot" component={ForgotPassword} />
         <Route path="/study" component={TeacherList} />
         <Route path="/give-classes" component={TeacherForm} />
       </Switch>
