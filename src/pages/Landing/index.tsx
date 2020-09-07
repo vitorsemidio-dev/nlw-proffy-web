d8d0d40d450009aaeca55e009fcf69bff3f69b13
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-09-07 13:37:48 -0300

    Estrutura header

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -11,6 +11,14 @@ import giveClassesIcon from '../../assets/images/icons/give-classes.svg';
 import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg';
 
 import './styles.css';
+import {
+  HeaderContainer,
+  HeaderContent,
+  ProfileInfoContainer,
+  AvatarImage,
+  UserName,
+  Logout,
+} from './styles';
 
 interface Connection {
   total: number;
@@ -27,37 +35,48 @@ const Landing: React.FC = () => {
   }, [setTotalConnections]);
 
   return (
-    <div id="page-landing">
-      <div id="page-landing-content" className="container">
-        <div className="logo-container">
-          <img src={logoImg} alt="Proffy" />
+    <>
+      <HeaderContainer>
+        <HeaderContent>
+          <ProfileInfoContainer>
+            <AvatarImage />
+            <UserName>Usuario</UserName>
+          </ProfileInfoContainer>
+          <Logout />
+        </HeaderContent>
+      </HeaderContainer>
+      <div id="page-landing">
+        <div id="page-landing-content" className="container">
+          <div className="logo-container">
+            <img src={logoImg} alt="Proffy" />
 
-          <h2>Sua plataforma de estudos online</h2>
+            <h2>Sua plataforma de estudos online</h2>
+          </div>
+
+          <img
+            src={landingImg}
+            alt="Plataforma de estudos"
+            className="hero-image"
+          />
+
+          <div className="buttons-container">
+            <Link to="/study" className="study">
+              <img src={studyIcon} alt="Estudar" />
+              Estudar
+            </Link>
+            <Link to="/give-classes" className="give-classes">
+              <img src={giveClassesIcon} alt="Dar aulas" />
+              Dar aulas
+            </Link>
+          </div>
+
+          <span className="total-connections">
+            Total de {totalConnections} conexões já realizadas{' '}
+            <img src={purpleHeartIcon} alt="Coração Roxo" />
+          </span>
         </div>
-
-        <img
-          src={landingImg}
-          alt="Plataforma de estudos"
-          className="hero-image"
-        />
-
-        <div className="buttons-container">
-          <Link to="/study" className="study">
-            <img src={studyIcon} alt="Estudar" />
-            Estudar
-          </Link>
-          <Link to="/give-classes" className="give-classes">
-            <img src={giveClassesIcon} alt="Dar aulas" />
-            Dar aulas
-          </Link>
-        </div>
-
-        <span className="total-connections">
-          Total de {totalConnections} conexões já realizadas{' '}
-          <img src={purpleHeartIcon} alt="Coração Roxo" />
-        </span>
       </div>
-    </div>
+    </>
   );
 };
 
---
+++ src/pages/Landing/styles.ts
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+export const HeaderContainer = styled.div`
+  width: 100%;
+  position: fixed;
+  top: 0;
+  height: 6.4rem;
+  background-color: white;
+`;
+
+export const HeaderContent = styled.header`
+  margin: 0 auto;
+  max-width: 110rem;
+  background: #88d5af;
+  height: 100%;
+`;
+
+export const ProfileInfoContainer = styled.div``;
+
+export const AvatarImage = styled.img``;
+
+export const UserName = styled.strong``;
+
+export const Logout = styled.button``;
