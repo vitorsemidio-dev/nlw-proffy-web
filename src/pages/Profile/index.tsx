2ddcbecddca4e6e9b4d67c5ce06cf66873aaf9fd
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-09-07 17:16:29 -0300

    AvatarInput

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,18 +1,24 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { Link } from 'react-router-dom';
 
 import logoImg from '../../assets/images/logo.svg';
 import backIcon from '../../assets/images/icons/back.svg';
+import cameraIcon from '../../assets/images/icons/camera.svg';
 
 import {
   Container,
   HeaderContainer,
   HeaderContent,
   SectionInfo,
+  AvatarInput,
   FormContainer,
 } from './styles';
 
 const Profile: React.FC = () => {
+  const handleAvatarChange = useCallback(() => {
+    console.log('handleAvatarChange');
+  }, []);
+
   return (
     <Container>
       <HeaderContainer>
@@ -24,7 +30,19 @@ const Profile: React.FC = () => {
           <img src={logoImg} alt="Proffy" />
         </HeaderContent>
       </HeaderContainer>
-      <SectionInfo>Info</SectionInfo>
+      <SectionInfo>
+        <AvatarInput>
+          <img
+            src="https://api.adorable.io/avatars/48/abott@adorable.png"
+            alt="Avatar"
+          />
+
+          <label htmlFor="avatar">
+            <img src={cameraIcon} alt="Camera Input" />
+            <input type="file" id="avatar" onChange={handleAvatarChange} />
+          </label>
+        </AvatarInput>
+      </SectionInfo>
 
       <FormContainer>Form</FormContainer>
     </Container>
--- src/pages/Profile/styles.ts
+++ src/pages/Profile/styles.ts
@@ -50,4 +50,59 @@ export const SectionInfo = styled.section`
   display: block;
 `;
 
+export const AvatarInput = styled.div`
+  margin-bottom: 32px;
+  position: relative;
+  width: 186px;
+  margin: 0 auto;
+
+  align-self: center;
+
+  > img {
+    width: 186px;
+    height: 186px;
+    border-radius: 50%;
+  }
+
+  label {
+    /* Position */
+    position: absolute;
+    right: 0;
+    bottom: 0;
+
+    /* Size */
+    width: 48px;
+    height: 48px;
+
+    /* Styles */
+    background: var(--color-secondary);
+    border: 0;
+    border-radius: 50%;
+    cursor: pointer;
+
+    /* Flex */
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    /* Child */
+    img {
+      width: 3.2rem;
+      height: 3.2rem;
+      color: #312e38;
+    }
+
+    input {
+      display: none;
+    }
+
+    /* Animation */
+    transition: background-color 0.2s;
+
+    &:hover {
+      background-color: var(--color-secondary-dark);
+    }
+  }
+`;
+
 export const FormContainer = styled.main``;
